64ec551b11c3e672dbeebe6428b7c34e24008c63
Keith Brings <keith.brings@noizu.com>
2018-09-17 04:37:55 +0700

    Updating dependencies and incrementing version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Noizu.KitchenSink.Mixfile do
 
   def project do
     [app: :noizu_kitchen_sink,
-     version: "0.1.6",
+     version: "0.1.7",
      elixir: "~> 1.4",
      package: package(),
      deps: deps(),
@@ -29,9 +29,9 @@ defmodule Noizu.KitchenSink.Mixfile do
       {:ex_doc, "~> 0.16.2", only: [:dev], optional: true}, # Documentation Provider
       {:markdown, github: "devinus/markdown", only: [:dev], optional: true}, # Markdown processor for ex_doc
       {:uuid, "~> 1.1" },
-      {:noizu_core, github: "noizu/ElixirCore", tag: "1.0.4", override: true},
-      {:noizu_simple_pool, github: "noizu/SimplePool", tag: "1.3.29"},
-      {:noizu_scaffolding, github: "noizu/ElixirScaffolding", tag: "1.1.33"},
+      {:noizu_core, github: "noizu/ElixirCore", tag: "1.0.5", override: true},
+      {:noizu_simple_pool, github: "noizu/SimplePool", tag: "1.3.30"},
+      {:noizu_scaffolding, github: "noizu/ElixirScaffolding", tag: "1.1.35"},
       {:noizu_mnesia_versioning, github: "noizu/MnesiaVersioning", tag: "0.1.8"},
       {:timex, "~> 3.1.24"}, # Date/Time library
       {:sendgrid, github: "SolaceClub/sendgrid_elixir", tag: "v1.8.0-templates"}, # Derived from Sendgrid Api Wrapper (https://github.com/alexgaribay/sendgrid_elixir)
