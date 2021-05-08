ceded0c04a46bd1bd67bd5fecda431eeb4465b7a
Keith Brings <keith.brings@noizu.com>
2021-05-09 03:20:50 +0700

    Version increment

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Noizu.KitchenSink.Mixfile do
 
   def project do
     [app: :noizu_kitchen_sink,
-     version: "0.3.0",
+     version: "0.3.1",
      elixir: "~> 1.4",
      package: package(),
      deps: deps(),
