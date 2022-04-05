6ae8dfde3347edc8add345aee5f45cffada27a5a
Keith Brings <keith.brings@noizu.com>
2022-04-05 11:30:19 +0700

    Minor build/dep cleanup

--- mix.exs
+++ mix.exs
@@ -3,14 +3,15 @@ defmodule Noizu.KitchenSink.Mixfile do
 
   def project do
     [app: :noizu_kitchen_sink,
-     version: "0.3.9",
-     elixir: "~> 1.4",
-     package: package(),
-     deps: deps(),
-     description: "Noizu Kitchen Sink",
-     docs: docs(),
-     elixirc_paths: elixirc_paths(Mix.env),
-   ]
+      version: "0.3.10",
+      elixir: "~> 1.13",
+      package: package(),
+      deps: deps(),
+      description: "Noizu Kitchen Sink",
+      docs: docs(),
+      xref: [exclude: [UUID]],
+      elixirc_paths: elixirc_paths(Mix.env),
+    ]
   end # end project
 
   defp package do
@@ -27,7 +28,7 @@ defmodule Noizu.KitchenSink.Mixfile do
       applications: [:logger],
       extra_applications: [
         :timex, :sendgrid, :amnesia, :fastglobal, :semaphore, :noizu_mnesia_versioning,
-        :noizu_core, :noizu_scaffolding, :noizu_simple_pool, :uuid,
+        :noizu_core, :noizu_scaffolding, :noizu_simple_pool,
         :noizu_rule_engine, :poison, :markdown]
 
     ]
@@ -35,23 +36,20 @@ defmodule Noizu.KitchenSink.Mixfile do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.16.2", only: [:dev], optional: true}, # Documentation Provider
+      {:ex_doc, "~> 0.28.3", only: [:dev], optional: true}, # Documentation Provider
       {:markdown, github: "devinus/markdown", optional: false}, # Markdown processor for ex_doc
-      {:uuid, "~> 1.1" },
-      {:poison, "~> 3.1.0"}, # HTTP Operations
-      {:exquisite, git: "https://github.com/noizu/exquisite.git", ref: "7a4a03d", override: true},
-      {:amnesia, git: "https://github.com/noizu/amnesia.git", ref: "9266002", override: true}, # Mnesia Wrappeir
-      {:noizu_core, github: "noizu/ElixirCore", tag: "1.0.7", override: true},
-      {:noizu_simple_pool, github: "noizu/SimplePool", tag: "2.1.1", override: true},
-      {:noizu_scaffolding, github: "noizu/ElixirScaffolding", tag: "1.2.1", override: true},
-      {:noizu_mnesia_versioning, github: "noizu/MnesiaVersioning", tag: "0.1.9", override: true},
-      {:noizu_rule_engine, github: "noizu/RuleEngine", tag: "0.2.0"},
+      {:noizu_simple_pool, github: "noizu/SimplePool", tag: "2.2.2"},
+      {:noizu_rule_engine, github: "noizu/RuleEngine", tag: "0.2.1"},
       {:fastglobal, "~> 1.0"}, # https://github.com/discordapp/fastglobal
       {:semaphore, "~> 1.0"}, # https://github.com/discordapp/semaphore
-      {:tzdata, github: "noizu/tzdata", tag: "opt_exp", override: true},
-      {:timex, github: "noizu/timex", ref: "7e3c887", override: true},
+      {:tzdata, "~> 1.1"},
+      {:timex, "~> 3.7"},
       {:sendgrid, github: "Noizu/sendgrid_elixir", branch: "master"},
       {:mock, "~> 0.3.1", optional: true},
+      {:poison, "~> 3.1.0", optional: true},
+      {:plug, "~> 1.0", optional: true},
+      {:elixir_uuid, "~> 1.2", optional: true},
+      {:redix, github: "whatyouhide/redix", tag: "v0.7.0", optional: true},
     ]
   end # end deps
 
